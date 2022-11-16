bc6d06d7008b7ab8ae9b2c8974aa389699067bad
Sonia Orive <isavo70@hotmail.com>
2022-11-16 19:19:54 +0100

    Method of data cleaning

--- php/storeDataForm.php
+++ php/storeDataForm.php
@@ -1,5 +1,37 @@
 <?php
-    
+// Clean strings to prevent sql injection
+function cleanString($textString)
+{
+    $textString = trim($textString);
+    $textString = stripslashes($textString);
+    $textString = str_ireplace("<script>", "", $textString);
+    $textString = str_ireplace("</script>", "", $textString);
+    $textString = str_ireplace("<script src>", "", $textString);
+    $textString = str_ireplace("</script type>", "", $textString);
+    $textString = str_ireplace("SELECT * FROM", "", $textString);
+    $textString = str_ireplace("DELETE * FROM", "", $textString);
+    $textString = str_ireplace("INSERT INTO", "", $textString);
+    $textString = str_ireplace("DROP TABLE", "", $textString);
+    $textString = str_ireplace("DROP DATABASE", "", $textString);
+    $textString = str_ireplace("TRUNCATE TABLE", "", $textString);
+    $textString = str_ireplace("SHOW TABLES", "", $textString);
+    $textString = str_ireplace("SHOW DATABASES", "", $textString);
+    $textString = str_ireplace("<?php", "", $textString);
+    $textString = str_ireplace("?>", "", $textString);
+    $textString = str_ireplace("--", "", $textString);
+    $textString = str_ireplace("^", "", $textString);
+    $textString = str_ireplace("<", "", $textString);
+    $textString = str_ireplace("[", "", $textString);
+    $textString = str_ireplace("]", "", $textString);
+    $textString = str_ireplace("==", "", $textString);
+    $textString = str_ireplace(";", "", $textString);
+    $textString = str_ireplace("::", "", $textString);
+    $textString = trim($textString);
+    $textString = stripslashes($textString);
+
+    return $textString;
+}   
+  
 
 
 
