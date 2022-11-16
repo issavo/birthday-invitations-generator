73401348b622f0b498c1feb8d4d855bf6129587f
Sonia Orive <isavo70@hotmail.com>
2022-11-16 12:28:07 +0100

    Create function for sending values stored in variables

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const formAjax = document.querySelector(".ajaxForm");
+
+function sendFormAjax(e){
+    e.preventDefault();
+    let name = document.getElementById("name_user").value;
+    let date = document.getElementById("date_user").value;
+    let time = document.getElementById("time_user").value;
+    let localName = document.getElementById("localName_user").value;
+    let address = document.getElementById("address_user").value;
+    let content = document.querySelector(".message");
+    
+   
+    
+}
+
+formAjax.addEventListener("submit", sendFormAjax);
