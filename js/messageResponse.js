135f61033c636893d19c08fb126c38c53741ddf1
Sonia Orive <isavo70@hotmail.com>
2022-11-18 11:22:15 +0100

    Change invitation url

--- js/messageResponse.js
+++ js/messageResponse.js
@@ -12,8 +12,8 @@ const values = urlParams.values();
 for (let value of values) {
   let valuesParam = JSON.parse(value);
   url.innerHTML =
-    "<div class='container pt-3 pb-5'><h5 class='pt-3 pb-2'>El enlace de tu invitación es:</h5><a class='pt-2 pb-5' href='../index.html'>" +
-    `./index.html?name=${valuesParam.name_user}&fecha=${valuesParam.date_user}&time=${valuesParam.time_user}&local=${valuesParam.localName_user}&address=${valuesParam.address_user}` +
+    "<div class='container pt-3 pb-5'><h5 class='pt-3 pb-2'>El enlace de tu invitación es:</h5><a class='pt-2 pb-5' href='./invitation.php'>" +
+    `./invitation.php?name=${valuesParam.name_user}&fecha=${valuesParam.date_user}&time=${valuesParam.time_user}&local=${valuesParam.localName_user}&address=${valuesParam.address_user}` +
     "</a></div>";
 }
 
