97b81cf9e63b8bf940ee455aace2bad70d88373a
Sonia Orive <isavo70@hotmail.com>
2022-11-18 08:16:59 +0100

    Storing data

--- php/storeDataForm.php
+++ php/storeDataForm.php
@@ -1,4 +1,41 @@
 <?php
+
+//Storing data in variables
+    $name = cleanString($_POST['name_user']);
+    $date = cleanString($_POST['date_user']);
+    $time = cleanString($_POST['time_user']);
+    $local = cleanString($_POST['localName_user']);
+    $address = cleanString($_POST['address_user']);
+    $response = [];
+    $errors = [];
+    
+//Data verification
+    if($name == "" || $date == "" || $time == "" || $local == "" || $address == ""){ $errors += "Debe rellenar los campos del formulario"; 
+   } else {
+        $response = [
+        "name_user" => $name,
+        "date_user" => $date,
+        "time_user" => $time,
+        "localName_user" => $local,
+        "address_user" => $address
+        ];
+        echo json_encode($response);
+    }
+
+    if(isset($errors)){
+    ?>
+        <div class="alert alert-danger" role="alert">
+            <strong>Ops!Ha habido un error<br/></strong>
+            <?php 
+                foreach($errors as $error){
+                    echo $error;
+                }
+            ?>   
+        </div>
+        
+    <?php 
+    }
+
 // Clean strings to prevent sql injection
 function cleanString($textString)
 {
