105c24cc80e19434c035fa430627aa7809e818ab
Sonia Orive <isavo70@hotmail.com>
2022-11-18 11:20:28 +0100

    Invitation structure

---
+++ views/invitation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Invitación de Cumpleaños</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../img/favicon.ico" />
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap-formhelpers.min.css">
+        <!-- Main CSS -->
+        <link rel="stylesheet" href="../css/style.css" />
+    </head>
+    <body>
+        <!-- Header Start -->
+        <section class="bg-body-invitation">
+            <div class="header-title">
+                <h1>Invitación de Cumpleaños</h1>
+            </div>
+            <div class="container bg-invitation">
+                <div class="row">
+                    <div class="col-md-10 col-lg-8 col-xl-7 mx-auto text-center">
+                        <div class="content-invitation">
+                            <h2>Celébralo con nosotros</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Header End -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../js/bootstrap-formhelpers.min.js"></script>
+    </body>
+
+</html>
