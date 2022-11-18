f006281f8710bc98ba312fa10a76b3eabe220175
Sonia Orive <isavo70@hotmail.com>
2022-11-18 11:25:05 +0100

    Include file containing form data

--- views/invitation.php
+++ views/invitation.php
@@ -1,6 +1,9 @@
+<?php
+    require_once "../php/storeDataForm.php";
+?>
+
 <!DOCTYPE html>
 <html lang="es">
-
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
