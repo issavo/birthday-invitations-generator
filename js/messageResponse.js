2316864d16ab9c6affe3ddc37c7e97aa15b5a58c
Sonia Orive <isavo70@hotmail.com>
2022-11-18 06:28:40 +0100

    Add javascript file to show succes message and link to view the invitation

---
+++ js/messageResponse.js
@@ -0,0 +1,11 @@
+let content = document.querySelector(".message");
+content.innerHTML =
+  "<div class='alert alert-success pt-3 pb-3' role='alert'>" +
+  "<h2>ENHORABUENA!!</h2>" +
+  "Los datos han sido registrados correctamente. " +
+  "</div>";
+
+
+
+
+     
